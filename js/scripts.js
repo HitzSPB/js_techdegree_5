fc431804b8a0a999877134ac6aafc9774e778b89
HitzSPB <daniel.oesterbro@gmail.com>
2021-06-26 12:14:25 +0200

    Added more content to javascript (modal and so on)

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+let fetchedData;
+
 // Adding the template code rom index.html.
 document.querySelector('.search-container').innerHTML = `<form action="#" method="get">
 <input type="search" id="search-input" class="search-input" placeholder="Search...">
@@ -10,6 +12,7 @@ document.querySelector('.search-container').innerHTML = `<form action="#" method
 var result = fetch('https://randomuser.me/api/?results=12&nat=us')
 .then(CheckResponse)
 .then(data => data.json())
+.then(data => fetchedData = data)
 .then(data => PasteDataIntoGallaryElement(data))
 
 
@@ -26,54 +29,61 @@ function CheckResponse(data){
 function PasteDataIntoGallaryElement(data){
     // Clear the current gallary content
     document.querySelector('#gallery').innerHTML = "";
-
+let count = 0;
 data.results.forEach(element => {
     // Creates a div that contains a profile card with the information from the fetch api
     var profileCardDiv = document.createElement('div');
     // Sets the data into the html structure
-    profileCardDiv.innerHTML = `<div class="card">
-    <div class="card-img-container">
+    profileCardDiv.classList.add("card");
+    profileCardDiv.id = `${count}`;
+    profileCardDiv.innerHTML = `<div class="card-img-container">
         <img class="card-img" src="${element.picture.large}" alt="profile picture">
     </div>
     <div class="card-info-container">
         <h3 id="name" class="card-name cap">${element.name.first} ${element.name.last}</h3>
         <p class="card-text">${element.email}</p>
         <p class="card-text cap">${element.location.city}, ${element.location.state}</p>
-    </div>
     </div>`
+
+    // Add event listner to card element
+    profileCardDiv.addEventListener('click', (event) => {
+        ShowModal(parseInt(event.target.closest(".card").id));
+    });
 
     // Append our profileCardDiv to the gallary
     document.querySelector('#gallery').appendChild(profileCardDiv);
+    count++;
 });
 }
 
-
-// // Adding modal
-// document.body.innerHTML = document.body.innerHTML + `<div class="modal-container">
-// <div class="modal">
-//     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-//     <div class="modal-info-container">
-//         <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-//         <h3 id="name" class="modal-name cap">name</h3>
-//         <p class="modal-text">email</p>
-//         <p class="modal-text cap">city</p>
-//         <hr>
-//         <p class="modal-text">(555) 555-5555</p>
-//         <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-//         <p class="modal-text">Birthday: 10/21/2015</p>
-//     </div>
-// </div>
-
-// // IMPORTANT: Below is only for exceeds tasks 
-// <div class="modal-btn-container">
-//     <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-//     <button type="button" id="modal-next" class="modal-next btn">Next</button>
-// </div>
-// </div>`
-
-
 // Function to pop out the modal
-function ShowModal()
+function ShowModal(currentId)
 {
+    let data = fetchedData.results[currentId]
+    document.body.innerHTML = document.body.innerHTML + `<div class="modal-container">
+    <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+            <img class="modal-img" src="${data.picture.large}" alt="profile picture">
+            <h3 id="name" class="modal-name cap">${data.name.first} ${data.name.last}</h3>
+            <p class="modal-text">${data.email}</p>
+            <p class="modal-text cap">${data.location.city}</p>
+            <hr>
+            <p class="modal-text">${data.cell.replace("-", " ")}</p>
+            <p class="modal-text">${data.location.street.number} ${data.location.street.name}, ${data.location.city}, ${data.location.state}, ${data.location.postcode}</p>
+            <p class="modal-text">Birthday: ${new Date(data.dob.date).toLocaleString("en-US").replaceAll("/","-").split(',')[0]}</p>
+        </div>
+    </div>
+    
+    // IMPORTANT: Below is only for exceeds tasks 
+    <div class="modal-btn-container">
+        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+        <button type="button" id="modal-next" class="modal-next btn">Next</button>
+    </div>
+    </div>`
 
+    document.querySelector("#modal-close-btn").addEventListener("click", (event) => 
+    {
+        document.body.removeChild(document.querySelector(".modal-container"));
+    })
 }
