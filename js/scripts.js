5b9de1f7e5bd92eaff053a7ba5183f0d7f8064c7
HitzSPB <daniel.oesterbro@gmail.com>
2021-06-26 10:32:52 +0200

    Added info to readme and added profilecard generation from the randomuser api

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,49 @@ document.querySelector('.search-container').innerHTML = `<form action="#" method
 <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
 </form>`
 
-// Adding a profile card to the website
-document.querySelector('#gallery').innerHTML = `<div class="card">
-<div class="card-img-container">
-    <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-</div>
-<div class="card-info-container">
-    <h3 id="name" class="card-name cap">first last</h3>
-    <p class="card-text">email</p>
-    <p class="card-text cap">city, state</p>
-</div>
-</div>`
+
+
+// Fetch data from random user api
+var result = fetch('https://randomuser.me/api/?results=12&nat=us')
+.then(CheckResponse)
+.then(data => data.json())
+.then(data => PasteDataIntoGallaryElement(data))
+
+
+// Checking if the response status from the randomuser api get correct data and resolves the promise that fetch creates.
+function CheckResponse(data){
+    if (data.ok) {
+        return Promise.resolve(data)
+    } else{
+        return Promise.reject(new Error(data.statusText));
+    }
+}
+
+// Creates the elements from the fetch response with the HTML structure from the index.html page
+function PasteDataIntoGallaryElement(data){
+    // Clear the current gallary content
+    document.querySelector('#gallery').innerHTML = "";
+
+data.results.forEach(element => {
+    // Creates a div that contains a profile card with the information from the fetch api
+    var profileCardDiv = document.createElement('div');
+    // Sets the data into the html structure
+    profileCardDiv.innerHTML = `<div class="card">
+    <div class="card-img-container">
+        <img class="card-img" src="${element.picture.large}" alt="profile picture">
+    </div>
+    <div class="card-info-container">
+        <h3 id="name" class="card-name cap">${element.name.first} ${element.name.last}</h3>
+        <p class="card-text">${element.email}</p>
+        <p class="card-text cap">${element.location.city}, ${element.location.state}</p>
+    </div>
+    </div>`
+
+    // Append our profileCardDiv to the gallary
+    document.querySelector('#gallery').appendChild(profileCardDiv);
+});
+}
+
 
 // // Adding modal
 // document.body.innerHTML = document.body.innerHTML + `<div class="modal-container">
@@ -38,3 +70,10 @@ document.querySelector('#gallery').innerHTML = `<div class="card">
 //     <button type="button" id="modal-next" class="modal-next btn">Next</button>
 // </div>
 // </div>`
+
+
+// Function to pop out the modal
+function ShowModal()
+{
+
+}
